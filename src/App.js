8107ac1b19517eb16cde1a44a0696912bd169900
karthickmurugan33 <karrthick33@gmail.com>
2023-03-04 22:07:23 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-
-import './App.css';
-import Nav from "../src/components/Nav"
-import UserDetail from './components/UserDetail';
-import Footer from './components/Footer';
-import Map from './components/Map';
+import "./App.css";
+import Nav from "../src/components/Nav";
+import UserDetail from "./components/UserDetail";
+import Footer from "./components/Footer";
+import Map from "./components/Map";
+import Page3 from "./components/Page3";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <UserDetail />
-      <Map />
-      <Footer />
+      <BrowserRouter>
+        <Switch>
+          <Route path={"/"} exact>
+            <Nav />
+            <UserDetail />
+            <Map />
+            <Footer />
+          </Route>
+
+          <Route path={"/page3"} exact>
+            <Page3 />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
